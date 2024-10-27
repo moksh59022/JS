5f255647a58c1329abd9794d8eae51d35c4d1843
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-27 12:08:50 +0000

    conversions and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,65 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "jj";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0 => false
+//"" => false
+//"kjk" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//------------------operations-------------------
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = "moksh"
+
+
+
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCount = 1;
+
+
+
+// let ans1 = ++gameCount
+// let ans11 = gameCount
+// console.log(ans1);
+// console.log(ans11);
+
+
+let ans2 = gameCount++
+let ans3 = gameCount;
+console.log(ans2);
+console.log(ans3);
+
+
+
