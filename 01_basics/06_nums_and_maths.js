cccdacf48b80c20fc9886b08b6fcfd0fea9c98d5
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-27 14:50:00 +0000

    maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,48 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.length)
+// console.log(balance.toFixed(2))
+
+// const otherNo = 23.939393
+// console.log(otherNo.toPrecision(4));
+// const otherNo1 = 1123.939393
+// console.log(otherNo1.toPrecision(2));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// let a = Number.MAX_VALUE
+// console.log(a.toLocaleString('en-IN'));
+// let b = Number.MIN_VALUE
+// console.log(b);
+
+//-------------------Maths--------------------------
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(0,3,5,3,4,6,6,3,2,4,5,4,53,42,3,23,23))
+// console.log(Math.max(0,3,5,3,4,6,6,3,2,4,5,4,53,42,3,23,23))
+
+
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+1))
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1)+min));
+
+
+
+
+
+
+
