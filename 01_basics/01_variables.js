8bafc4bc2931f6c3c45cfdeebab65cf0c02be136
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-27 10:58:16 +0000

    variables And Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1433
+let accountEmail = "abc@gmail.com"
+var accountPassword = 123
+accountCity = "HP"
+let accountState;
+
+console.log(accountCity);
+console.log([accountId , accountEmail ,accountCity]);
+console.table([accountId , accountEmail ,accountCity ,accountState]);
+
