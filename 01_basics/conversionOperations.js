512eecb0680648f12e8f8f8b2a201ffb47ae9a7d
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-27 12:02:03 +0000

    conversions and operations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = "jj";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true ; 0 => false
@@ -25,6 +25,41 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//------------------operations-------------------
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = "moksh"
+
+
+
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCount = 1;
+
+
+
+// let ans1 = ++gameCount
+// let ans11 = gameCount
+// console.log(ans1);
+// console.log(ans11);
+
+
+let ans2 = gameCount++
+let ans3 = gameCount;
+console.log(ans2);
+console.log(ans3);
+
+
 
