2f05be91a9fdf1cdb4458b2367086f09a57e7685
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-27 12:16:36 +0000

    comparison operators

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null<=0);
+
+// comparisons convert null to a number , treating it as 0
+// thaths why null<=0 is true and null==0 is false
+
+console.log(undefined==0);
+console.log(undefined<0);
+console.log(undefined>=0);
