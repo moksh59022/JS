544fe28cbb224232f0f445d01e1df318ea3986e1
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-29 07:19:41 +0000

    switch

---
+++ 04_controlFlow.js/one.js
@@ -0,0 +1,49 @@
+// if
+const balance = 1000
+
+// if (balance>500) console.log("yes"),console.log("hlo");
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("jan");        
+        break;
+    case 2:
+        console.log("jan");        
+        break;
+    case 3:
+        console.log("jan");        
+        break;
+    case 4:
+        console.log("jan");        
+        break;
+    case 5:
+        console.log("jan");        
+        break;
+    case 6:
+        console.log("jan");        
+        break;
+    case 7:
+        console.log("jan");        
+        break;
+    case 8:
+        console.log("jan");        
+        break;
+    case 9:
+        console.log("jan");        
+        break;
+    case 10:
+        console.log("jan");        
+        break;
+    case 11:
+        console.log("jan");        
+        break;
+    case 12:
+        console.log("jan");        
+        break;
+
+    default:
+        console.log("enter valid number");
+        
+        break;
+}
