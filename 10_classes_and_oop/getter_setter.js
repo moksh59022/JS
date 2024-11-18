19157ad8881706fa797345ff68163c3b0b1434b4
moksh59022 <vsmokshsharma688@gmail.com>
2024-11-18 08:07:30 +0000

    getter setter

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,22 @@
+class User{
+    constructor(email,password){
+        this.email=email;
+        this.password=password
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+         this._email=value
+    }
+
+    get password(){
+         return this._password.toUpperCase()
+    }
+    set password(value){
+          this._password=value
+    }
+}
+
+const moksh = new User("moksh@ai","123")
+console.log(moksh.password);
